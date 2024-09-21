9f434137db1f5dcc9b9a062f85d0a4f61f8fcbaa
zain <zainrajpootfd@gmail.com>
2024-09-21 12:35:37 +0500

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+// promises method
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// try catch method
+
+// const asyncHandler = (fn) => async () => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Server Error", success: false });
+//   }
+// };
